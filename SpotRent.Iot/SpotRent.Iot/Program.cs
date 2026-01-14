acc48746ebcbed67398df4cd3d0e0d97611ce29b
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2026-01-14 14:26:56 +0200

    chore: rm iot

--- SpotRent.Iot/SpotRent.Iot/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using SpotRent.Iot;
-
-var options = DeviceOptions.Load("device-settings.json");
-var device = new Device(options);
-device.AddSmartLock(options.InitialLockState);
-
-await device.RunAsync();
